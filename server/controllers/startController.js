00307f8cea0f9a0dc222dc9003e62e9498257a7f
Harvey <harveyjwilliams@aol.co.uk>
2019-12-21 18:23:58 +0100

    Basic edit page for seeing the used schema.

--- server/controllers/startController.js
+++ server/controllers/startController.js
@@ -1,14 +1,40 @@
 const logger = require('../logger');
 const app = require('../app');
+const path = require('path');
+const appDir = path.dirname(require.main.filename);
+const fs = require('fs');
 
 module.exports = function() {
-    // Render startpage.
-    // app.get('/:page', function(req, res) {
-    //     logger.info(req.params.page);
+    app.get('/start/:name/edit', function (req, res, next) {
+        const name = req.params.name;
 
-    //     res.render('index', {
-    //         layout: 'common',
-    //         relativeUrl: '',
-    //     });
-    // });
+        const schema = fs.readFileSync('./pages/' + name + '/manifest/schema.json', 'utf8');
+
+        res.render('edit', {
+            layout: 'common',
+            relativeUrl: '',
+            schema: schema,
+        });
+    });
+
+    // Render startpage.
+    app.get('/start/*', function (req, res, next) {
+        const options = {
+            root: path.join(appDir, 'pages'),
+            dotfiles: 'deny',
+            headers: {
+                'x-timestamp': Date.now(),
+                'x-sent': true
+            }
+        };
+
+        const filePath = req.params[0];
+
+        logger.info(filePath);
+
+        res.sendFile(filePath, options, function (err) {
+            if (err)
+                next(err)
+        });
+    });
 };
