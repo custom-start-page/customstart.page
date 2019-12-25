6bc00c5ef28a88433e3eb51c76234200c2aab842
Harvey <harveyjwilliams@aol.co.uk>
2019-12-25 23:11:05 +0100

    Fixed SUI?

--- pages/sui/assets/js/data.js
+++ pages/sui/assets/js/data.js
@@ -1,29 +1,21 @@
-var data_links = "links.json";
-  
+const data = new Storage('data-sui').get();
+
 $(document).ready(function(){
-  $.getJSON(data_links,
-    function (data) {
-      var mysource = $('#links-template').html();
-      var mytemplate = Handlebars.compile(mysource);
-      var myresult = mytemplate(data)
-      $('#links').html(myresult);
-  });
+  var mysource = $('#links-template').html();
+  var mytemplate = Handlebars.compile(mysource);
+  var myresult = mytemplate(data)
+  $('#links').html(myresult);
 });
 
-var data_apps = "apps.json";
-  
 $(document).ready(function(){
-  $.getJSON(data_apps,
-    function (data) {
-      var mysource = $('#apps-template').html();
-      var mytemplate = Handlebars.compile(mysource);
-      var myresult = mytemplate(data)
-      $('#apps').html(myresult);
-  });
+  var mysource = $('#apps-template').html();
+  var mytemplate = Handlebars.compile(mysource);
+  var myresult = mytemplate(data)
+  $('#apps').html(myresult);
 });
 
 var data_providers = "providers.json";
-  
+
 $(document).ready(function(){
   $.getJSON(data_providers,
     function (data) {
