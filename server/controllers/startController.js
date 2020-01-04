0aac1e1dc793abc5d21e5dab88a84cb1d29e134a
Harvey <harveyjwilliams@aol.co.uk>
2020-01-04 22:14:34 +0100

    Improving templates.

--- server/controllers/startController.js
+++ server/controllers/startController.js
@@ -29,7 +29,7 @@ module.exports = function() {
             layout: 'common',
             relativeUrl: '',
             themeName: themeName,
-            hideFooter: true,
+            hideFooter: false,
         });
     });
 
