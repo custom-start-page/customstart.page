323bce238ca61a5b5f45a2c726ab007028eab4c1
harvzor <harveyjwilliams@aol.co.uk>
2026-04-18 17:10:28 +0200

    Remove IIS/IISNode support

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 global.config = {
     domains: process.env.DOMAINS.split(',').map(d => d.trim()),
     port: parseInt(process.env.PORT),
-    type: process.env.TYPE,
     useServiceWorker: process.env.USE_SERVICE_WORKER === 'true',
     dev: process.env.DEV === 'true',
     basedir: __dirname,
@@ -20,17 +19,9 @@ logger.info(global.config);
 // Inititialise
 /////////////////
 
-if (global.config.type === 'node') {
-    // Used for Node server.
-    var server = app.listen(global.config.port, function () {
-        const host = server.address().address;
-        const port = server.address().port;
+var server = app.listen(global.config.port, function () {
+    const host = server.address().address;
+    const port = server.address().port;
 
-        logger.info(`Website listening at http://${host}:${port}.`);
-    });
-} else if (global.config.type === 'iis') {
-    // Used for IISNode.
-    app.listen(process.env.PORT);
-} else {
-    logger.error('Error: wrong TYPE env var set');
-}
+    logger.info(`Website listening at http://${host}:${port}.`);
+});
